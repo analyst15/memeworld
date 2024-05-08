8223f438cb58daa1865c34ea3a1ca8591cde57be
Alex Okeyo <alexokeyo15@gmail.com>
2024-05-08 16:54:10 +0300

    New Updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-        <TwoinOne :name="'MEME.'" :imgsrc="home" :visit="'/about'" :btnName="'Get Started'"/>
+        <TwoinOne :name="'MEME'" :imgsrc1="home" :imgsrc2="google" :imgsrc3="apple" :visit="'/about'" :btnName="'Get Started'"/>
     </div>
 </template>
 
 <script setup>
 import TwoinOne from '../components/TwoinOne.vue';
 import home from '../assets/images/group89.png'
+import google from '../assets/images/GooglePlay.png'
+import apple from '../assets/images/AppStore.png'
 </script>
 
 <style scoped>
