eb633ca039b9726a1ad62fa2602108bf122d7dfd
Alex Okeyo <alexokeyo15@gmail.com>
2024-05-09 14:30:57 +0300

    Added footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+<footer class="bg-orange-500 text-white py-8 px-4">
+
+</footer>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
     <div>
         <TwoinOne :name="'MEME'" :imgsrc1="home" :imgsrc2="google" :imgsrc3="apple" :visit="'/about'" :btnName="'Get Started'"/>
         <Body />
+        <Footer />
     </div>
 </template>
 
@@ -11,6 +12,7 @@ import home from '../assets/images/group89.png'
 import Body from '../components/Body.vue';
 import google from '../assets/images/GooglePlay.png'
 import apple from '../assets/images/AppStore.png'
+import Footer from '../components/Footer.vue'
 </script>
 
 <style scoped>
