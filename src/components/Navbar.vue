f61a5109b9c7594281eb86bda6dc692d1b7b61ef
Alex Okeyo <alexokkeyo15@gmail.com>
2024-05-12 00:46:58 +0300

    Humburger menu styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,10 +4,10 @@
             <div class="text-3xl font-extrabold text-blue-600 hover:text-blue-700 cursor-pointer">
                 <router-link to="/"> <img src="../assets/images/logo.png" class="h-10" alt="Logo"> </router-link> 
             </div>
-            <nav class="flex flex-col md:flex-row p-5 gap-4 md:gap-10 px-6 md:static absolute bg-orange-500 md:bg-transparent shadow-md md:shadow-none md:w-auto my-7 md:my-0 left-0 w-full duration-500" :class="[open ? 'top-0' : 'top-[-390%]']">
-                <router-link to="/blog" class="text-white py-5 hover:text-slate-700 text-base font-nunito font-normal">Blog</router-link>
-                <router-link to="/features" class="text-white py-5 hover:text-slate-700 text-base font-nunito font-normal">Features</router-link>
-                <router-link to="/contact" class="text-white py-5 hover:text-slate-700 text-base font-nunito font-normal">Contact Us</router-link>
+            <nav class="flex flex-col md:flex-row p-5 gap-4 md:gap-10 px-6 md:static absolute bg-orange-500 md:bg-transparent shadow-md md:shadow-none md:w-auto my-3 md:my-0 left-0 w-full duration-500" :class="[open ? 'top-0' : 'top-[-390%]']">
+                <router-link to="/blog" class="text-white py-1 hover:text-slate-700 text-base font-nunito font-normal">Blog</router-link>
+                <router-link to="/features" class="text-white py-1 hover:text-slate-700 text-base font-nunito font-normal">Features</router-link>
+                <router-link to="/contact" class="text-white py-1 hover:text-slate-700 text-base font-nunito font-normal">Contact Us</router-link>
             </nav>
             <div class="test-4xl text-white cursor-pointer md:hidden z-20" @click="menuOpen">
                 <i :class="[open ? 'text-3xl bi bi-x' : ' text-3xl bi bi-list' ]"></i>
